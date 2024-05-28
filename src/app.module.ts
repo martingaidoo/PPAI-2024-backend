11e58387ee000fa16aa0fbef2f256958730006a0
MartinCesano <cesano305@gmail.com>
2024-05-28 19:16:49 -0300

    Genere los modulos y las entity para poder trabajar organizadamente, ahora falta codear todo xd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GenerarReporteRankingVinosModule } from './Modules/generar-reporte-ranking-vinos/generar-reporte-ranking-vinos.module';
 import { GenerarReporteRankingVinosController } from './Modules/generar-reporte-ranking-vinos/generar-reporte-ranking-vinos.controller';
+import { PaisModule } from './Modules/pais/pais.module';
+import { ProvinciaModule } from './Modules/provincia/provincia.module';
+import { RegionVitivinicolaModule } from './Modules/region-vitivinicola/region-vitivinicola.module';
+import { ReseñaModule } from './Modules/reseña/reseña.module';
+import { TipoReseñaModule } from './Modules/tipo-reseña/tipo-reseña.module';
+import { VinoModule } from './Modules/vino/vino.module';
 
 @Module({
-  imports: [GenerarReporteRankingVinosModule],
+  imports: [GenerarReporteRankingVinosModule, PaisModule, ProvinciaModule, RegionVitivinicolaModule, ReseñaModule, TipoReseñaModule, VinoModule],
   controllers: [AppController, GenerarReporteRankingVinosController],
   providers: [AppService],
 })
