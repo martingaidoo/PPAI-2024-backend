97b7def0c0e7dc13a2236a98f98f98732b8cbeb7
MartinCesano <cesano305@gmail.com>
2024-11-23 01:12:36 -0300

    Bonvino aplication actualizado

--- src/main/java/com/bonvino/BonvinoApplication.java
+++ src/main/java/com/bonvino/BonvinoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan(basePackages = "com.bonvino")
 public class BonvinoApplication {
-	//
 
 	public static void main(String[] args) {
 		SpringApplication.run(BonvinoApplication.class, args);
