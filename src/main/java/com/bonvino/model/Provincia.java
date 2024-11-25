0f6f38b477fd78c8fdbca0635e1b4b2cd6abfd25
MartinCesano <cesano305@gmail.com>
2024-11-25 02:32:01 -0300

    Funcionalidad terminada

--- src/main/java/com/bonvino/model/Provincia.java
+++ src/main/java/com/bonvino/model/Provincia.java
@@ -3,6 +3,7 @@ package com.bonvino.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import jakarta.persistence.*;
 import java.util.List;
 
@@ -17,6 +18,8 @@ public class Provincia {
     private String nombre;
     @ManyToOne(targetEntity = Pais.class)
     private Pais pais;
+
+    @ToString.Exclude
     @OneToMany(targetEntity = RegionVitivinicola.class, fetch = FetchType.LAZY, mappedBy = "provincia")
     private List<RegionVitivinicola> regionesVitivinicolas;
 
